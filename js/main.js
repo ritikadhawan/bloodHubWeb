a69519824a7033ef3afb385a82caeaf012263499
vibhutikathuria <vibhutikathuria99@gmail.com>
2020-01-31 22:53:56 +0530

    lund khop dungi

--- js/main.js
+++ js/main.js
@@ -1,15 +1,37 @@
+var messagesRef=firebase.database().ref('messages')
+
+
 document.getElementById('search-blood-form').addEventListener('submit', submitForm);
 
 function submitForm(e){
     e.preventDefault();
+    var name = inputVal('blood-type');
+    var state = inputVal('state');
+    var city = inputVal('city');
 
+    saveMessage(blood-type, state, city)
+
+}
+
+
+
+function inputVal(id){
+    return document.getElementById(id).value;
 
 
 
 }
+function saveMessage(blood-type, state, city){
+    var newMessageRef=messagesRef.push();
+    newMessageRef.set({
+        blood-type:blood-type,
+        state:state,
+        city:city
 
-function inputVal(id){
-    return document.getElementById(id).value;
+
+    })
+
+
 
 
 
