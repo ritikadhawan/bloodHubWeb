c38b17dbc9569a940811e9b712d6a74513e1a0b5
vibhutikathuria <vibhutikathuria99@gmail.com>
2020-01-31 20:18:16 +0530

    gaand mara bc

---
+++ js/main.js
@@ -0,0 +1,16 @@
+document.getElementById('search-blood-form').addEventListener('submit', submitForm);
+
+function submitForm(e){
+    e.preventDefault();
+
+
+
+
+}
+
+function inputVal(id){
+    return document.getElementById(id).value;
+
+
+
+}
